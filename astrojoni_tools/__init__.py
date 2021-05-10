77d503e936fd988b9a8ce1ff52afd1ca012ae327
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-05-10 13:06:21 +0200

    Create __init__.py

---
+++ astrojoni_tools/__init__.py
@@ -0,0 +1,11 @@
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+__minimum_python_version__ = "3.4"
+
+class UnsupportedPythonError(Exception):
+    pass
+
+if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
+    raise UnsupportedPythonError("astrojoni_tools does not support Python < {}".format(__minimum_python_version__))
