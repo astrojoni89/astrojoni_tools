6fb7828998b284b205517fa85d9730e1c07aafd5
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-06-28 14:29:33 +0200

    Create ism.py

---
+++ astrojoni_tools/ism.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def tau_hisa(T_hisa, p, T_on, T_off, T_cont):
+    return -np.log(1-((T_on-T_off)/(T_hisa-T_cont-p*T_off)))
+
+def hi_coldens(T_s, tau, dv):
+    return 1.8224 * 10**18 * T_s * tau * dv
+
