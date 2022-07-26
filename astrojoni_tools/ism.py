f4ad9dd6db59b6a12b4c79bd4fc20fa42ebe2799
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-07-26 17:12:12 +0200

    Update ism.py

--- astrojoni_tools/ism.py
+++ astrojoni_tools/ism.py
@@ -6,5 +6,5 @@ def tau_hisa(T_hisa, p, T_on, T_off, T_cont):
 def hi_coldens(T_s, tau, dv):
     return 1.8224 * 10**18 * T_s * tau * dv
 
-def T_hisa_max(p, T_on, T_off, T_cont):
+def t_hisa_max(p, T_on, T_off, T_cont):
     return T_on + T_cont - (1-p) * T_off
