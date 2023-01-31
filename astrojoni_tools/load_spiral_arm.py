4ed987fbcf18a3f622f6ff725acb0f4870629391
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2023-01-31 14:39:24 +0100

    Update load_spiral_arm.py

--- astrojoni_tools/load_spiral_arm.py
+++ astrojoni_tools/load_spiral_arm.py
@@ -2,10 +2,10 @@ import pkg_resources
 from astropy.table import Table, Column
 
 
-def load_data(filename):
+def load_data(filename, model='Reid2019'):
     """Return an astropy.Table containing spiral arm data.
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/{}'.format(filename))
+    stream = pkg_resources.resource_stream(__name__, 'data/{}/{}'.format(model,filename))
     return Table.read(stream, format='ascii')
