171010bbe4ae67821fe0397b86721c32e7e89654
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2023-01-31 15:02:12 +0100

    Update load_spiral_arm.py

--- astrojoni_tools/load_spiral_arm.py
+++ astrojoni_tools/load_spiral_arm.py
@@ -1,7 +1,15 @@
 import pkg_resources
+import glob
 from astropy.table import Table, Column
 
 
+def get_file_names(model='Reid2019'):
+    """Load the spiral arm file names.
+    """
+    stream = pkg_resources.resource_stream(__name__, 'data/{}/*.dat'.format(model))
+    return glob.glob(stream)
+    
+    
 def load_data(filename, model='Reid2019'):
     """Return an astropy.Table containing spiral arm data.
     """
