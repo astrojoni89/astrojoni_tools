44769bdb42da7d830c510b31d3cde4fda360c648
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2023-02-06 12:36:19 +0100

    Update plotting.py

--- astrojoni_tools/plotting.py
+++ astrojoni_tools/plotting.py
@@ -12,29 +12,29 @@ from .functions import find_nearest, velocity_axes, pixel_to_world, pixel_circle
 
 
 ### SCALEBAR PLOTTING IMSHOW
-def plot_scalebar(length, fitsfile, distance_of_source, loc='bottom right', labelcolor='white', labelsize='small', offset=0.05, unit='pc', **kwargs):
+def plot_scalebar(length, fitsfile, distance_of_source, ax=None, loc='bottom right', labelcolor='white', labelsize='small', offset=0.05, unit='pc', **kwargs):
     """This function plots a scalebar onto an existing figure axis.
     
     Parameters
     ----------
     length : float
-        Length of scalebar that is plotted on the axis object. Should have the same unit as 'distance_of_source'.
+        Length (in units of a projected physical scale; like '1.' [AU/pc/ly]) of scalebar that is plotted on the axis object. Should have the same unit as 'distance_of_source'.
     fitsfile : str
         Path to FITS file.
     distance_of_source : float
-        Distance of the plotted source; should have the same unit as 'length'.
+        Distance of the plotted source. Should have the same unit as 'length'.
     loc : str
         Location of scalebar. The default is 'bottom right'.
     labelcolor : str
         Color of scalebar label.
     labelsize : str or float
         Fontsize of the label.
-    offset : float
+    offset : float, optional
         Offset between scalebar an corresponding label. Given in units of axis fraction.
     unit : str
         Unit of scalebar label that is plotted.
     **kwargs : str or float
-        Keyword arguments that are passed to .plot()
+        Keyword arguments that are passed to ax.plot() of the actual scalebar.
     """
     loc_dict = {
         'bottom left': [0.03,0.15],
@@ -52,7 +52,8 @@ def plot_scalebar(length, fitsfile, distance_of_source, loc='bottom right', labe
     #length of scalebar (as well in unit u)
     pxscalebar = int(np.around(length / pxscale,decimals=0))
     
-    ax = plt.gca()
+    if ax is None:
+        ax = plt.gca()
     axis_to_data = ax.transAxes + ax.transData.inverted()
     
     if loc not in loc_dict:
@@ -74,7 +75,7 @@ def plot_scalebar(length, fitsfile, distance_of_source, loc='bottom right', labe
     #label depends on loc
     if unit == 'pc' or unit == 'parsec':
         ax.text(x_label, offset_label_data[1], '{} pc'.format(length), color=labelcolor, ha='center', va='top', family='serif', size=labelsize)
-    elif uni == 'au' or unit == 'AU':
+    elif unit == 'au' or unit == 'AU':
         ax.text(x_label, offset_label_data[1], '{} AU'.format(length), color=labelcolor, ha='center', va='top', family='serif', size=labelsize)
     elif unit == 'ly' or unit == 'lightyear':
         ax.text(x_label, offset_label_data[1], '{} ly'.format(length), color=labelcolor, ha='center', va='top', family='serif', size=labelsize)
